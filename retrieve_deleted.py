07f81daacf4dacb1a426d7f9a45cf1bbcacbee68
IgnacioBecerra <i1becerr@ucsd.edu>
2019-01-18 23:47:11 -0800

    Adding code to main file

--- retrieve_deleted.py
+++ retrieve_deleted.py
@@ -1,30 +1,44 @@
 import mechanize
 import sys
 import json
+import time
 import re
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options 
 import selenium.webdriver.support.ui as ui
 import time
-chrome_options = Options()  
+chrome_options = Options()
 #chrome_options.add_argument("--headless")
+
+
+start_time = time.time()
 br = webdriver.Chrome(chrome_options=chrome_options, executable_path=r'./chromedriver.exe')
 br.get('https://www.youtube.com/playlist?list=FLfu72fp0-EvF0_PMq2bLgQA&disable_polymer=1')
-wait = ui.WebDriverWait(br,50)
+wait = ui.WebDriverWait(br,3)
+
+def chunkErrorCheck():
+
+	while True:
+		try:
+			text = br.find_element_by_xpath('//*[@id="main-message"]/h1/span')
+
+			if text:
+				br.find_element_by_xpath('//*[@id="reload-button"]').click()
+		except:
+			break
+
 
 
 count = 0
 video_list = []
 
+# Click next button to load more videos
 for i in range(1, 5):
 	time.sleep(1)
 	button = wait.until(lambda br: br.find_element_by_xpath('//*[@id="pl-video-list"]/button')).click()
 	
-
-
-
+# Go through each video title, and collect the ID's of removed videos
 soup = BeautifulSoup(br.page_source, "html.parser")
 for tr in soup.findAll('tr'):
 	
@@ -37,18 +51,6 @@ for tr in soup.findAll('tr'):
 			print tr['data-title'] + ' - ' + tr['data-video-id']
 			video_list.append(tr['data-video-id'])
 	count = count + 1
-
-
-	# Load next batch of videos
-	#video = br.find_element_by_xpath('//*[@id="pl-load-more-destination"]/tr['+str(count)+']')
-	#button = wait.until(lambda br: br.find_element_by_xpath('//*[@id="pl-video-list"]/button'))
-
-	#if video:
-
-	#	button.click()
-	#	count = 0
-
-
 
 snapshots = []
 remaining = []
@@ -73,25 +75,36 @@ print('\n'.join(snapshots))
 # Scrape Wayback Machine for video titles using found video IDs
 for video in snapshots:
 	br.get(video)
+	chunkErrorCheck()
 	title = ''
+	
 	try:
 		title = br.find_element_by_xpath("//meta[@name='title']").get_attribute('content')
-		continue
+		
+		# Go to next video if title is found
+		if title != "":
+			print title
+			continue
 	except:
 		pass
 	
 	# Setting maximum tries if Wayback loops back (it's a glitchy website)
-	x = br.find_element_by_xpath('//*[@id="wm-nav-captures"]/a').get_attribute('text')
-	totalSnaps = re.findall('\d+', x[0])
+	x = wait.until(lambda br: br.find_element_by_xpath('//*[@id="wm-nav-captures"]/a').get_attribute('text'))
+	totalSnaps = re.findall('\d+', x)
 	totalSnaps = int(totalSnaps[0])
 	tries = 0
 
 	# Go to earliest snapshot instead if most recent is unavailable
 	while title == "" and tries < totalSnaps:
+		chunkErrorCheck()
 
 		# Get title 
 		try:
 			title = br.find_element_by_xpath("//meta[@name='title']").get_attribute('content')
+
+			if title != "":
+				print title
+				continue
 		except:
 			pass
 		
@@ -104,15 +117,17 @@ for video in snapshots:
 		tries = tries + 1
 
 	# Print current title for confirmation
-	if title:
-		print title
-	else:
+	if title == "":
+		print "appending..."
 		remaining.append(video)
 
+elapsed_time = time.time() - start_time
+print elapsed_time
 
 for video in remaining:
-	url = 'https://www.youtube.com/watch?v=' + video + '&disable_polymer=1'
+	url = video
 	br.get(url)
+	chunkErrorCheck()
 
 	# Get remains of title
 	title = br.find_element_by_xpath('//*[@id="unavailable-message"]').text
@@ -122,5 +137,5 @@ for video in remaining:
 	title = title + " " + video
 	print title
 
-
+elapsed_time = time.time() - start_time
 br.quit()
